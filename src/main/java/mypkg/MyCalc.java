021ad6104dce8df482a26acfd043fbdc01f4852b
Dr. Hitesh Kumar Sharma <hkshitesh@gmail.com>
2024-04-14 12:49:06 +0530

    maven project

---
+++ src/main/java/mypkg/MyCalc.java
@@ -0,0 +1,18 @@
+package mypkg;
+
+public class MyCalc {
+	public int sum(int a, int b)
+	{
+		return a+b;
+	}
+	public int diff(int a, int b)
+	{
+		return a-b;
+	}
+
+	public static void main(String[] args) {
+		MyCalc calc = new MyCalc();
+		System.out.println("Sum is "+calc.sum(10, 20));	
+		System.out.println("Diff is "+calc.diff(10, 20));
+	}
+}
