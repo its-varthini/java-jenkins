594905eeb0ef4024ffb00293a030d8697bb961a2
Dr. Hitesh Kumar Sharma <hkshitesh@gmail.com>
2024-04-14 12:56:06 +0530

    mul added

--- src/main/java/mypkg/MyCalc.java
+++ src/main/java/mypkg/MyCalc.java
@@ -9,10 +9,15 @@ public class MyCalc {
 	{
 		return a-b;
 	}
+	public int mul(int a, int b)
+	{
+		return a*b;
+	}
 
 	public static void main(String[] args) {
 		MyCalc calc = new MyCalc();
 		System.out.println("Sum is "+calc.sum(10, 20));	
 		System.out.println("Diff is "+calc.diff(10, 20));
+		System.out.println("Diff is "+calc.mul(10, 20));
 	}
 }
